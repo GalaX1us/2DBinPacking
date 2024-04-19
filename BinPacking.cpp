95ea752ccaf0f5bb56e7fa83b4fb5a925f7b041b
Nathan BONADA <nathan.bonada@gmail.com>
2024-04-19 08:25:18 +0200

    Ajoutez des fichiers projet.

---
+++ BinPacking.cpp
@@ -0,0 +1,12 @@
+﻿// BinPacking.cpp : définit le point d'entrée de l'application.
+//
+
+#include "BinPacking.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
