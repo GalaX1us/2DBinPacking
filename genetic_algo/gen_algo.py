dba39847350a8c3ee4dc6e8944c5d52dd45de217
Nathan <nathan.bonada@gmail.com>
2024-05-16 23:56:00 +0200

    new: base of the main algo

--- genetic_algo/gen_algo.py
+++ genetic_algo/gen_algo.py
@@ -1,24 +1,32 @@
 import numpy as np
-from solutions_generation import generate_initial_population
-from genetic_algo.structures import Item
+from crossover import crossover
+from solutions_generation import generate_population
+from structures import Item, create_item
 
 
-# def genetic_algo(items, bin_dimensions, population_size, nb_generations, crossover_rate, kappa, delta):
-#     population = generate_initial_population(population_size, kappa)
+def genetic_algo(items, bin_dimensions, population_size, nb_generations, crossover_rate, kappa, delta):
     
-#     for i in range(nb_generations):
-#         new_population = crossover(population, crossover_rate, delta)
+    population = generate_population(items, population_size, kappa)
+    
+    # Unpack the bin dimensions
+    bin_width, bin_height = bin_dimensions
+    
+    for i in range(nb_generations):
+        
+        crossover_population = crossover(population, bin_width, bin_height, crossover_rate, delta)
+        remaining_population = generate_population(items, population_size - crossover_population.shape[0] ,kappa)
+        population = np.concatenate([crossover_population, remaining_population])
 
 if __name__ == "__main__":
     import time
     items = np.array([
-        (0, 10, 20, False),
-        (1, 30, 40, False),
-        (2, 50, 60, False),
-        (3, 70, 80, False),
+        create_item(0, 10, 20),
+        create_item(1, 30, 40),
+        create_item(2, 50, 60, False),
+        create_item(3, 70, 80, False),
         ], dtype=Item)
     
-    pop = generate_initial_population(items, 10, 1)
+    pop = generate_population(items, 10, 0.5)
     
     print(pop)
     
