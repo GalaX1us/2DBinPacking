20e47514c4964a2b29e52a02a5f8efba40af2f78
Nathan <nathan.bonada@gmail.com>
2024-05-19 20:23:19 +0200

    advanced compilation logging

--- genetic_algo/gen_algo.py
+++ genetic_algo/gen_algo.py
@@ -1,11 +1,13 @@
 import time
 from typing import Tuple
-from genetic_algo.mutation import mutation
+
+import tqdm
+from genetic_algo.mutation import *
 from genetic_algo.solutions_generation import *
-from genetic_algo.crossover import crossover
+from genetic_algo.crossover import *
 from genetic_algo.structures import *
-from genetic_algo.fitness import compute_fitnesses
-from genetic_algo.lgfi import lgfi
+from genetic_algo.fitness import *
+from genetic_algo.lgfi import *
 
 def genetic_algo(items: np.ndarray,
                  bin_dimensions: Tuple[int, int],
@@ -36,55 +38,107 @@ def genetic_algo(items: np.ndarray,
     - tuple: A tuple containing the best solution found and its corresponding fitness.
     """
     
-    # Unpack the bin dimensions
-    bin_width, bin_height = bin_dimensions
-    
     population = generate_population(items, population_size, kappa)
     
     best_solution = np.zeros_like(population[0])
-    best_fitness = 0
+    best_fitness = np.inf
     
     
-    for _ in range(nb_generations):
+    for _ in tqdm.tqdm(range(nb_generations)):
         
-        fitnesses = compute_fitnesses(population, items, bin_width, bin_height, guillotine_cut, rotation)
+        fitnesses = compute_fitnesses(population, items, bin_dimensions, guillotine_cut, rotation)
         
         # Store best generation
-        best_index = np.argmax(fitnesses)
+        best_index = np.argmin(fitnesses)
         current_best_fitness = fitnesses[best_index]
-        if current_best_fitness > best_fitness:
+        if current_best_fitness < best_fitness:
             best_fitness = current_best_fitness
             best_solution[:] = population[best_index]
         
         
-        crossover_population = crossover(population, fitnesses, crossover_rate, delta)
+        crossover_population = crossover(population, fitnesses, items, crossover_rate, delta)
         remaining_population = generate_population(items, population_size - crossover_population.shape[0] ,kappa)
         population = np.concatenate([crossover_population, remaining_population])
         
-        # population = mutation(population, mutation_rate)
+        population = mutation(population, mutation_rate)
         
     return best_solution, best_fitness 
+    
+def initialize_numba_functions(advanced=False):
+    """
+    Initialize Numba-compiled functions with dummy arguments and measure compilation time.
+    This function compiles all specified functions into machine code using Numba and caches the results for faster execution in future calls.
 
-def compile_everything() -> None:
+    Parameters:
+    - advanced (bool): If True, prints detailed compilation times for each function. Default is False.
     """
-    Compile all the functions in machine code with numba. Uses dummy data.
-    """
     
-    bin_width, bin_height = 10, 10
+    if advanced:
+        print("===================== Compilation (Advanced Mode) =====================")
+
+    bin = create_bin(1, 100, 100)
     
-    start = time.perf_counter()
+    # Fonctions à compiler avec leurs arguments
+    functions_with_args = {
+        create_bin: (1, 10, 10),
+        create_free_rectangle: (0, 0, 5, 5),
+        create_item: (1, 3, 3, False),
+        add_item_to_bin: (bin, create_item(0, 0, 0), 0, 0),
+        add_free_rect_to_bin: (bin, create_free_rectangle(0, 0, 0, 0)),
+        remove_free_rect_from_bin: (bin, create_free_rectangle(0, 0, 0, 0)),
+        remove_free_rect_from_bin_by_idx: (bin, 0),
+        get_item_by_id: (np.zeros(5, dtype=Item), 1),
+        custom_choice: (np.arange(5), np.random.random(5)),
+        remove_index: (np.arange(5), 2),
+        generate_population: (np.zeros(5, dtype=Item), 10, 0.5),
+        get_corresponding_sequence_by_id: (np.zeros(5, dtype=Item), np.arange(5)),
+        mutation: (np.zeros((5, 5), dtype=np.int32), 0.5),  
+        swap_individual: (np.arange(5),),  
+        rotate_individual: (np.arange(5),),  
+        remove_item_from_remaining: (np.zeros(5, dtype=Item), 1),  
+        spliting_process_guillotine: (True, bin, create_free_rectangle(0, 0, 0, 0), create_item(0, 0, 0)),  
+        merge_rec_guillotine: (bin,),  
+        handle_wastage: (bin, create_free_rectangle(0, 0, 0, 0), 0, 0),  
+        check_fit_and_rotation: (np.zeros(5, dtype=Item), 0, 0, True),  
+        perform_placement: (bin, create_free_rectangle(0, 0, 0, 0), create_item(0, 0, 0), True, 0, 0, True),  
+        insert_item_lgfi: (bin, np.zeros(5, dtype=Item), True, True),  
+        find_current_position_idx: (bin,),  
+        lgfi: (np.empty(0, dtype=Item), 10, 10, True, True),
+        calculate_bin_fill: (bin,),  
+        compute_fitness_fill: (np.zeros(5, dtype=Bin), 100),  
+        compute_fitness: (np.array([create_item(0, 6, 6), create_item(1, 6, 6), create_item(2, 4, 4), create_item(3, 3, 3)]), np.array([0, 1, 2, 3]), (10, 10), True, True),  
+        compute_fitnesses: (np.array([np.array([0, 1, 2])]), np.array([create_item(0, 6, 6),create_item(1, 6, 6),create_item(2, 4, 4)]), (10, 10), True, True),  
+        offspring_generation: (np.zeros(5, dtype=np.int32), np.zeros(5, dtype=np.int32), 0, 0),  
+        crossover: (np.zeros((1, 1), dtype=np.int32), np.random.random(1), np.zeros(1, dtype=Item), 0.5, 2.0),  
+    }
     
-    items = np.array([
-        create_item(0, 6, 6),
-        create_item(1, 6, 6), 
-    ])
+    total_compilation_time: float = 0.0
+    max_func_name_len = 0
+    compilation_times = {}
+
+    for func, args in (pbar := tqdm.tqdm(functions_with_args.items())):
+        
+        pbar.set_description(f"Compiling {func.__name__}", refresh=True)
+        
+        start_time = time.perf_counter()
+        func(*args)
+        end_time = time.perf_counter()
+        
+        function_name = func.__name__
+        max_func_name_len = max(max_func_name_len, len(function_name))
+        
+        comp_time = end_time - start_time
+        
+        compilation_times[func.__name__] = end_time - start_time
+        
+        total_compilation_time += comp_time
     
-    lgfi(items, bin_width, bin_height, True, True)
-    pop = generate_population(items, 5, 1.0)
-    fit = compute_fitnesses(pop, items, bin_width, bin_height, True, True)
-    crossover(pop, fit, 0.7, 1.0)
+    pbar.set_description(f"Finished !", refresh=True)
     
-    print("===========================")
-    print(f"Compilation took : {time.perf_counter() - start:.1f} sec")
-    print("===========================\n")
-    
+    if advanced:
+        for name, duration in compilation_times.items():
+            print(f"{name:>{max_func_name_len}} -> {duration:<8.4f} secondes")
+        
+    print("====================================================================")    
+    print(f"Total compilation time -> {total_compilation_time:.4f} seconds")
+    print("====================================================================\n")
