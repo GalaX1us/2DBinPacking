4fa52cd811107fa75cf454d6b7571a5e7cf4011d
Nathan <nathan.bonada@gmail.com>
2024-06-16 07:25:34 +0200

    feat: Cleaned Imports

--- main.py
+++ main.py
@@ -2,21 +2,16 @@ from numba import config
 
 # Easy way to disable or Enable JIT Compilation with Numba
 # Useful for performance comparison
-config.DISABLE_JIT = True
+config.DISABLE_JIT = False
 
-from binpacking.data_manager import export_solutions_to_json, import_solution_from_json, load_items_from_file
-from binpacking.genetic_algo.gen_algo import genetic_algo, initialize_numba_functions
-from binpacking.lgfi import lgfi
-from binpacking.population_generation import get_corresponding_sequence_by_id
-from binpacking.tabu_search import tabu_search
-from binpacking.visualization import visualize_bins
+from binpacking.genetic_algo.gen_algo import initialize_numba_functions
 from solutions_helper import generate_all_solutions, generate_single_solution, Metaheuristic, visualize_solution
     
 INPUT_DATA_DIRECTORY = "data"
 OUTPUT_DATA_DIRECTORY = "solutions"
 
 # Parameters for Genetic Algorithm
-POPULATION_SIZE = 10
+POPULATION_SIZE = 20
 NB_GENERATIONS = 1000
 CROSSOVER_RATE = 0.7
 
@@ -28,16 +23,18 @@ TABU_LIST_SIZE = 10
 
 # Genetic Parameters
 KAPPA = 5 # Must be >= 1 (For both GA and TABU)
-DELTA = 20 # Must be >= 1 (Only for GA)
+DELTA = 5 # Must be >= 1 (Only for GA)
 
 GUILLOTINE = True
 ROTATION = True
 
 SELECTED_METAHEURISTIC = Metaheuristic.GA
 
+assert KAPPA >= 1, "KAPPA must be >= 1"
+assert DELTA >= 1, "DELTA must be >= 1"
+
 if __name__ == "__main__":
 
-    
     # ====================== Compile code ======================
     # You should first execute this single line to compile everything and cache the compiled code
     # This allows future compilations time to be waaaaaaay faster
@@ -46,13 +43,13 @@ if __name__ == "__main__":
     
     # ====================== Generate All Solutions ======================
 
-    generate_all_solutions(SELECTED_METAHEURISTIC, ITERATION_NUMBER, TABU_LIST_SIZE, KAPPA, GUILLOTINE, ROTATION, 
-                           POPULATION_SIZE, NB_GENERATIONS, CROSSOVER_RATE, MUTATION_RATE, DELTA, 
-                           INPUT_DATA_DIRECTORY, OUTPUT_DATA_DIRECTORY)
+    # generate_all_solutions(SELECTED_METAHEURISTIC, ITERATION_NUMBER, TABU_LIST_SIZE, KAPPA, GUILLOTINE, ROTATION, 
+    #                        POPULATION_SIZE, NB_GENERATIONS, CROSSOVER_RATE, MUTATION_RATE, DELTA, 
+    #                        INPUT_DATA_DIRECTORY, OUTPUT_DATA_DIRECTORY)
             
     # =================== Generate One Solutions ==================
     
-    # file = "binpacking2d-04.bp2d" # Just chnage the dataset number to generate another solution
+    # file = "binpacking2d-11.bp2d" # Just chnage the dataset number to generate another solution
     
     # generate_single_solution(file, SELECTED_METAHEURISTIC, ITERATION_NUMBER, TABU_LIST_SIZE, KAPPA, GUILLOTINE, ROTATION,
     #                          POPULATION_SIZE, NB_GENERATIONS, CROSSOVER_RATE, MUTATION_RATE, DELTA, INPUT_DATA_DIRECTORY, OUTPUT_DATA_DIRECTORY)
@@ -60,5 +57,5 @@ if __name__ == "__main__":
     
     # ====================== Visualize Solutions ======================
     
-    file = "binpacking2d-04-solution.json" # Just chnage the solution number to visualize another solution
+    file = "binpacking2d-13-solution.json" # Just chnage the solution number to visualize another solution
     visualize_solution(file, OUTPUT_DATA_DIRECTORY)
